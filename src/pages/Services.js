66b1dd60b02506cb9ed9a41e641eeeb6a5147969
Mfaiz378 <105097686+Mfaiz378@users.noreply.github.com>
2025-06-26 23:18:35 +0530

    Update Services.js

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -17,7 +17,11 @@ function Services() {
       <h1>Our Services</h1>
       <div className="services-list">
         {services.map((service, idx) => (
-          <div className="service-card" style={{ animationDelay: `${idx * 0.3}s` }} key={idx}>
+          <div
+            className="service-card"
+            style={{ animationDelay: `${idx * 0.2}s` }}
+            key={idx}
+          >
             {service}
           </div>
         ))}
