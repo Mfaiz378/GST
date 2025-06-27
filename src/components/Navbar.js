75a71c30fcc756cb590f4e188016b433aced0aa5
Mfaiz378 <105097686+Mfaiz378@users.noreply.github.com>
2025-06-27 12:42:30 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="navbar">
-      <div>GST & Services</div>
+      <div>GST Returns Services</div>
       <div>
         <Link to="/">Home</Link>
         <Link to="/services">Services</Link>
